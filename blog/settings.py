f77644bc60d3d6fdb07e2c047445659a2a6f0da5
devbobnwaka <nwakachibueze@gmail.com>
2022-12-22 18:14:51 +0100

    setup postgre db

--- blog/settings.py
+++ blog/settings.py
@@ -89,7 +89,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+# DATABASES = {
+#      "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+#     }
 if not DEBUG:
     DATABASES = {
      "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
