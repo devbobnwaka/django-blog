eded7644c190b5e4e4efa7b9cdad668c445ab40e
devbobnwaka <nwakachibueze@gmail.com>
2022-12-22 17:15:49 +0100

    dev allowedhost edited

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ DEBUG = str(os.environ.get('DJANGO_DEBUG')) == '1'
 # DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000' , os.environ.get('DJANGO_ALLOWED_HOST')]
 if not DEBUG:
     ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
 
