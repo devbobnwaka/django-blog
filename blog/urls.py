6fffd49ad4ca857bdd1742f6e3844406aff06e24
devbobnwaka <nwakachibueze@gmail.com>
2022-12-22 22:25:45 +0100

    testing the url.py

--- blog/urls.py
+++ blog/urls.py
@@ -19,16 +19,24 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from django.views.static import serve 
 
+# urlpatterns = [
+#     path('', include('accounts.urls')),
+#     path('', include('administrators.urls')),
+#     path('', include('clients.urls')),
+#     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+#     path('admin/', admin.site.urls),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('administrators.urls')),
     path('', include('clients.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
 handler404 = "blog.views.page_not_found_view"
 
-# if settings.DEBUG:  
-#     #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+if settings.DEBUG:  
+    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
