2abc34b2be54ed64176f91fa9fd79c132a6d2faf
devbobnwaka <nwakachibueze@gmail.com>
2022-12-22 12:37:23 +0100

    Debug true

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == '1'
+# DEBUG = os.environ.get('DEBUG') == 1
+DEBUG = True
 
 ALLOWED_HOSTS = []
 if not DEBUG:
